14a6f5931cf2fe106095fec86dff55c5ecf0342b
afse84 <afse84@outlook.com>
2021-10-21 20:26:25 +1030

    Stop for learning PERN

--- server/jcmm.js
+++ server/jcmm.js
@@ -8,8 +8,9 @@ jcmm.use(express.json());
 jcmm.use(express.urlencoded({ extended: false }));
 
 jcmm.get('/login', (req, res) => {
-            console.log('Clicked on Server');
-            const value = 152;
-            res.json = ({ value: value });
+    console.log('Clicked on Server');
+    const value = 152;
+    res.json = ({ value: value });
+});
 
-            jcmm.listen(3000, console.log('JCMM Running on Port 3000'))
+jcmm.listen(3000, console.log('JCMM Running on Port 3000'))
